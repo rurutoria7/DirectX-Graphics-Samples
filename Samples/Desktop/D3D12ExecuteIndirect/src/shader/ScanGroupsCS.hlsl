f03e037a416fdf6d63ac62af10478f22f8a21d4d
rurutoria7 <nandokabeam@gmail.com>
2025-05-15 16:15:34 +0800

    1. 移除 legacy pipeline 中的 scanned_group_sum_buffer 2. 增加 DEBUG_CULL_DETERMINISTIC 模式

--- Samples/Desktop/D3D12ExecuteIndirect/src/shader/ScanGroupsCS.hlsl
+++ Samples/Desktop/D3D12ExecuteIndirect/src/shader/ScanGroupsCS.hlsl
@@ -5,13 +5,11 @@
 
 groupshared uint temp[NOOF_THREADS * 2];
 
-StructuredBuffer<my_uint> groupSumArrayIn : register(t0);
 RWStructuredBuffer<my_uint> groupSumArrayOut : register(u0);
 
 [numthreads(NOOF_THREADS, 1, 1)]
 [RootSignature(
     ROOT_SIG_B1
-    "SRV(t0), "
     "UAV(u0)"
 )]
 void scanGroupSums(uint3 threadID : SV_DispatchThreadID, uint3 groupThreadID : SV_GroupThreadID, uint3 groupID : SV_GroupID)
@@ -21,8 +19,8 @@ void scanGroupSums(uint3 threadID : SV_DispatchThreadID, uint3 groupThreadID : S
     const int NoofElements = NOOF_THREADS * 2;
 
     int offset = 1;
-    temp[2 * tID] = groupSumArrayIn[2 * tID].x;
-    temp[2 * tID + 1] = groupSumArrayIn[2 * tID + 1].x;
+    temp[2 * tID] = groupSumArrayOut[2 * tID].x;
+    temp[2 * tID + 1] = groupSumArrayOut[2 * tID + 1].x;
 
     int d;
 
